ddf5a0c6bdbd8971c50af2ded284ac7c61dd1b4a
sfc <sfc@18235931687.com>
2020-04-01 16:38:09 +0800

    '第一个flask项目'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def index():
-    return "<h1>Hello,Flask</h1>"
+    return "<h1>Hello,Flask chang</h1>"
+
+# 动态路由
+@app.route('/index/<name>')
+def home(name):
+    print(url_for('home',name='chang'))
+    return "<h1>Hello,%s</h1>"%name
