09fd5b2139383a0b62f239f8191cf6d5adb4ba46
sfc <sfc@18235931687.com>
2020-04-01 17:48:51 +0800

    '第一次flask项目'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "<h1>Hello,Flask chang</h1>"
+    name = 'Bruce'
+    movies = [
+        {"title":"大赢家","year":"2020"},
+        {"title":"速度与激情","year":"201"}
+    ]
+    return render_template('index.html',name=name,movies=movies)
 
-# 动态路由
-@app.route('/index/<name>')
-def home(name):
-    print(url_for('home',name='chang'))
-    return "<h1>Hello,%s</h1>"%name
