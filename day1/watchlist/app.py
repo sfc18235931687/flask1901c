1cf7a5b34db3e589a2406e5927d896e146066b82
sfc <sfc@18235931687.com>
2020-04-02 16:10:11 +0800

    '数据库创建完成，数据库导入成功'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
-app.config['SQLALCHY_TRACK_MODIFICATIONS'] = Flask #关闭了对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭了对模型修改的监控
 db = SQLAlchemy(app) #初始化拓展，传入程序实例app
 
 # models
@@ -32,16 +32,15 @@ class Movie(db.Model):
 @app.route('/')
 
 def index():
-    name = 'Bruce'
-    movies = [
-        {"title":"大赢家","year":"2020"},
-        {"title":"速度与激情","year":"201"}
-    ]
-    return render_template('index.html',name=name,movies=movies)
+   
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
 
 
 # 自定义命令
+# 新建data.db的数据库初始化命令
 @app.cli.command()  # 装饰器，注册命令
 @click.option('--drop',is_flag=True,help='删除后再创建')
 def initdb(drop):
@@ -49,3 +48,22 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('初始化数据库完成')
+
+# 向data.db中写入数据的命令
+@app.cli.command()
+def forge():
+    
+
+    name = 'Bruce'
+    movies = [
+        {"title":"大赢家","year":"2020"},
+        {"title":"速度与激情","year":"201"}
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('插入数据完成')
