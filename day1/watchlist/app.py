bdef505b0ac63cd091eacd83fa81cf952d06bd97
sfc <sfc@18235931687.com>
2020-04-03 13:26:28 +0800

    '表单完成'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,7 +1,7 @@
 import os
 import sys 
 
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,request,url_for
 import click
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
@@ -16,6 +16,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭了对模型修改的监控
+app.config['SECRET_KEY'] = 'watchlist_dev'
 db = SQLAlchemy(app) #初始化拓展，传入程序实例app
 
 # models
@@ -34,13 +35,43 @@ def common_user():
     return dict(user=user)
 
 # views
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 
 def index():
-   
-    
+    if request.method == 'POST':
+        # request再请求触发的时候才会包含数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('不能为空或超过最大的长度')
+            return redirect(url_for('index'))
+        # 保存表单数据
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('创建成功')
+        return redirect(url_for('index'))
+
     movies = Movie.query.all()
     return render_template('index.html',movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        # 验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('不能为空或超过最大的长度')
+            return redirect(url_for('index'))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('更新完成')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
 
 
 
