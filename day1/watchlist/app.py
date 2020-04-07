0209ec6fcb5870f0c5151858c8c35b1d8523efd2
sfc <sfc@18235931687.com>
2020-04-07 17:01:42 +0800

    'flask项目完成'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -5,7 +5,7 @@ from flask import Flask,render_template,flash,redirect,request,url_for
 from werkzeug.security import generate_password_hash,check_password_hash
 import click
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
-from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required,current_user
 
 
 WIN = sys.platform.startswith('win')
@@ -27,6 +27,7 @@ login_manager = LoginManager(app) #实例化登录拓展类
 def load_user(user_id):
     user = User.query.get(int(user_id))
     return user
+login_manager.login_view = 'login'
 
 
 # models
@@ -57,6 +58,8 @@ def common_user():
 
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         # request再请求触发的时候才会包含数据
         title = request.form.get('title')
         year = request.form.get('year')
@@ -76,6 +79,7 @@ def index():
 
 # 更新电影信息
 @app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     if request.method == 'POST':
@@ -106,6 +110,7 @@ def delete(movie_id):
 
 # 登录
 @app.route('/login',methods=['GET','POST'])
+
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -132,6 +137,21 @@ def logout():
     logout_user()
     flash('拜拜')
     return redirect(url_for('index'))
+
+# settings 设置
+@app.route('/settings',methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+        if not name or len(name) > 20:
+            flash('输入错误')
+            return redirect(url_for('settings'))
+        current_user.name = name
+        db.session.commit()
+        flash('名称已经更新')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
 
 # 自定义命令
 # 新建data.db的数据库初始化命令
