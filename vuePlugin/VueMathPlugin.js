4c2defeb0df24d8eb1875a5b28018b6bdaf8939b
sfc <sfc@18235931687.com>
2020-04-13 17:27:29 +0800

    '自定义插件'

---
+++ vuePlugin/VueMathPlugin.js
@@ -0,0 +1,22 @@
+export default{
+    install:function(Vue){
+        // 平方
+        Vue.directive('square',function(el,binding){
+            el.innerHTML = Math.pow(binding.value,2)
+        });
+        // 开方
+        Vue.directive('sqrt',function(el,binding){
+            el.innerHTML = Math.sqrt(binding.value)
+        });
+        // 把item当做角度，计算sin、cos、tan值
+        Vue.directive('sin',function(el,binding){
+            el.innerHTML = Math.sin(binding.value)
+        })
+        Vue.directive('cos',function(el,binding){
+            el.innerHTML = Math.cos(binding.value)
+        })
+        Vue.directive('tan',function(el,binding){
+            el.innerHTML = Math.tan(binding.value)
+        })
+    }
+}
