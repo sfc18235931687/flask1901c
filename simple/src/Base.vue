de06331003857be2d95f7d06a689931f3cb03514
sfc <sfc@18235931687.com>
2020-04-15 17:44:17 +0800

    'index.vue'

--- simple/src/Base.vue
+++ simple/src/Base.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-    <div class="app-header">
-      <Header/>
+    
+    <div class="app-head">
+      <div class="app-head-inner">
+        <img src="./assets/logo.png" alt=""><span class='app-title'>海马生鲜商城</span>
+        <div class="head-nav">
+          <ul>
+            <li>登陆</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
     </div>
     <div class="app-content">
-      <Content/>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
     </div>
-    <div class="app-footer">
-      <Footer/>
+    <div class="app-foot">
+      <p>&copy;1901c - 2020</p>
     </div>
     
   </div>
@@ -16,26 +29,103 @@
 
 <script>
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Content from './components/Content'
+
 
 export default {
   data() {
     return {
-      msg: "基础模板"
+      
     };
   },
   components:{
-    Header,
-    Footer,
-    Content
+    
   }
 };
 </script>
 
-<style scoped>
-h1{
-  color:red
+<style>
+/* reset重置样式 */
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+/* base.vue 样式 */
+body{
+  background: #f0f2f5;
+  font-size:14px;
+  color:#444
+}
+.app-head{
+  width:100%;
+  height:100px;
+  line-height:100px;
+  color:#b2b2b2;
+  background: #363636;
+}
+.app-head-inner{
+  width:1200px;
+  margin:0 auto
+}
+.app-head-inner img{
+  width:50px;
+  margin-top: 25px;
+  float:left
+}
+.head-nav{
+  float:right
+}
+.head-nav li{
+  float:left;
+  cursor: pointer;
+}
+.nav-pile{
+  padding:0 10px;
+}
+.app-foot{
+  text-align: center;
+  height:80px;
+  line-height: 80px;
+  width:100%;
+  background: #e3e4e8;
+  margin-top:30px;
+}
+.app-content{
+  width:1200px;
+  margin: 0 auto;
+}
+.app-title{
+  font-size: 30px;
 }
 </style>
--- simple/src/main.js
+++ simple/src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Base from './Base.vue'
+import IndexPage from './components/index.vue'
 
+
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:"/",
+      component:IndexPage
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   components:{
     Base
   },
