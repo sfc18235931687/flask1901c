f6a537d3b67e292945c4e4644dda5944262651bc
sfc <sfc@18235931687.com>
2020-04-16 17:24:59 +0800

    '轮播图'

--- simple/src/mock/mock.js
+++ simple/src/mock/mock.js
@@ -9,4 +9,79 @@ Mock.mock(/getNewsList/,{
     ]
 })
 
+Mock.mock(/getProductList/, {
+    pc: {
+      title: "PC产品",
+      list: [{
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        }
+      ]
+    },
+    app: {
+      title: "手机应用类",
+      last: true,
+      list: [
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        },
+        {
+          title: "@ctitle(4)",
+          url: "@url",
+          hot: "@boolean"
+        }
+      ]
+    }
+  })
+  
+  Mock.mock(/getBoardList/,[
+      {
+          title:'@ctitle(4)',
+          description:'@ctitle(8,12)',
+          saleout:'@boolean'
+      },
+      {
+          title:'@ctitle(4)',
+          description:'@ctitle(8,12)',
+          saleout:'@boolean'
+      },
+      {
+          title:'@ctitle(4)',
+          description:'@ctitle(8,12)',
+          saleout:'@boolean'
+      },
+      {
+          title:'@ctitle(4)',
+          description:'@ctitle(8,12)',
+          saleout:'@boolean'
+      }
+  ])
 export default Mock
